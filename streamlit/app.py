df7b5f20b28bc170ff38f5a714713ba4909eca66
KIMjjjjjjjj <jin432101@gmail.com>
2025-11-04 03:20:41 +0900

    first commit

---
+++ streamlit/app.py
@@ -0,0 +1,17 @@
+# pip install streamlit
+# pip install streamlit-audiorecorder
+
+import streamlit as st
+
+def main():
+    st.set_page_config(
+        page_title='〈The Room of Lies〉',
+        page_icon="🕵️‍♀️",
+        layout='wide'
+    )
+    st.header('〈The Room of Lies〉')
+    st.markdown('---')
+
+
+if __name__ == '__main__':
+    main()
